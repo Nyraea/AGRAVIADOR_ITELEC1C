5f38eaf6df962218e78ed7cb390bb881aad34662
Nyraea <agraviador.markjearalds@gmail.com>
2023-09-05 18:33:47 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AGRAVIADOR_ITELEC1C.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
